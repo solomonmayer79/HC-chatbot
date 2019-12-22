a180a0aaf813163320f25c3ad330ff743b0a5dcf
solomon <sr17acm@herts.ac.uk>
2019-12-22 19:15:26 +0000

    add run file changes

--- HC-chatbot/run.py
+++ HC-chatbot/run.py
@@ -6,11 +6,36 @@ import os
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
+# database connection
+
+import psycopg2
+
+from datetime import datetime        
+
+today = datetime.today()
+
+connection = psycopg2.connect(user = "postgres",
+                                  password = "admin",
+                                  host = "127.0.0.1",
+                                  port = "5432",
+                                  database = "hc_chatbot")
+
+cursor = connection.cursor()
+
+select_Query = "select * from user_account"
+
+cursor.execute(select_Query)
+before_record_count = cursor.rowcount 
+#user_records = cursor.fetchall()
+
+#write conversation in the yaml file
 filenumber=int(os.listdir('saved_conversations')[-1])
 filenumber=filenumber+1
 file= open('saved_conversations/'+str(filenumber),"w+")
-file.write('bot : Hi There! I am a medical chatbot. You can begin conversation by typing in a message and pressing enter.\n')
+file.write('bot : Hi There! I am a Healthcare chatbot. You can begin conversation by typing in a message and pressing enter.\n')
 file.close()
+
+
 
 app = Flask(__name__)
 
@@ -26,24 +51,21 @@ english_bot = ChatBot('HealthCare',
 ],
 trainer='chatterbot.trainers.ListTrainer')
 english_bot.set_trainer(ListTrainer)
+english_bot.train("./data")
 
-# database connection
+#save the conversation to database
 
-import psycopg2
 
-connection = psycopg2.connect(user = "postgres",
-                                  password = "admin",
-                                  host = "127.0.0.1",
-                                  port = "5432",
-                                  database = "hc_chatbot")
+# create table
 
-cursor = connection.cursor()
+#create_Query = '''CREATE TABLE conversation(
+#    con_id serial PRIMARY KEY,
+#    user_id INT,
+#    message TEXT UNIQUE NOT NULL,
+#    start_on TIMESTAMP NOT NULL)'''
 
-select_Query = "select * from user_account"
-
-cursor.execute(select_Query)
-before_record_count = cursor.rowcount 
-#user_records = cursor.fetchall() 
+#cursor.execute(create_Query)
+#connection.commit()
 
 #@app.route("/")
 #def home():
@@ -52,10 +74,24 @@ before_record_count = cursor.rowcount
 @app.route('/')
 def home():
     if not session.get('logged_in'):
+       
         return render_template('login.html')
     else:
-        username = request.form['username']
-        return render_template("index.html", username = username)
+        #save conversation in database
+        username_db = request.form['username']
+        get_userID = "select * from user_account WHERE username = %s"
+        cursor.execute(get_userID, (username_db,))
+        userID_record= cursor.fetchone()
+        userID = userID_record[0]
+        today_db = datetime.now()
+
+        message_db = 'bot : Hi '+username_db+'! I am a Healthcare chatbot. You can begin conversation by typing in a message and pressing enter.'
+        
+        insert_query = """ INSERT INTO conversation (user_id, message, start_on) VALUES (%s,%s,%s)"""
+        record_to_insert = (userID, message_db, today_db)
+        cursor.execute(insert_query, record_to_insert)
+        connection.commit()
+        return render_template("index.html", username = username_db, uid = userID)
 
 @app.route("/new-user")
 def newUser():
@@ -67,13 +103,13 @@ def register():
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
+        address = request.form['address']
 
         # insert value in user account table
-        from datetime import datetime        
-        today = datetime.today()
+        
 
-        insert_query = """ INSERT INTO user_account (username, password, email, created_on, last_login) VALUES (%s,%s,%s,%s,%s)"""
-        record_to_insert = (username, password, email, today, today)
+        insert_query = """ INSERT INTO user_account (username, password, email, created_on, last_login, address) VALUES (%s,%s,%s,%s,%s,%s)"""
+        record_to_insert = (username, password, email, today, today, address)
         cursor.execute(insert_query, record_to_insert)
         connection.commit()
 
@@ -102,17 +138,42 @@ def do_admin_login():
     else:
         session['logged_in'] = True
         return home()  
-    
 @app.route("/get")
 def get_bot_response():
+    
     userText = request.args.get('msg')
     response = str(english_bot.get_response(userText))
 
+    #save conversation in database
+    #username = request.args.get('uname')
+    #print(username)
+    #username_home = request.form['username']
+    #uname = request.args.get('username_db')
+    #get_userID = "select * from user_account WHERE username = %s"
+    #cursor.execute(get_userID, (username,))
+    #userID_record= cursor.fetchone()
+    userID = request.args.get('userID') 
+    #print(userID)
+    #cursor.close()
+    #userID = userID_record[0]
+    message = 'user : '+userText+'\n'+'bot : '+response+'\n'
+
+    #update_query = """Update conversation set message = %s where user_id = %s"""
+    #cursor.execute(update_query, (message, userID))
+    #connection.commit()
+    
+    
     appendfile=os.listdir('saved_conversations')[-1]
     appendfile= open('saved_conversations/'+str(filenumber),"a")
     appendfile.write('user : '+userText+'\n')
     appendfile.write('bot : '+response+'\n')
     appendfile.close()
+    
+    print(message)
+    insert_query = """ INSERT INTO conversation ( message ) WHERE user_id = %s VALUES (%s)"""
+    record_to_insert = ( userID, message )
+    cursor.execute(insert_query, record_to_insert)
+    connection.commit()
 
     return response
 
